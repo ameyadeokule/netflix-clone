3a8da39ca5cb16c9684280cc522aaa1dc722cae7
Ameya Deokule <deokuleameya@gmail.com>
2021-05-03 18:17:31 +0530

    header component completed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { HeaderContainer } from '../containers/header'
 import { FaqsContainter } from '../containers/faq'
 import { FooterContainer } from '../containers/footer'
 import { JumbotronContainer } from '../containers/jumbotron'
+import { OptForm } from '../components'
 
 export default function Home() {
 	return (
 		<>
+			<HeaderContainer>
+				<OptForm>
+					<OptForm.Input placeholder='Email Address' />
+					<OptForm.Button>Try it Now</OptForm.Button>
+					<OptForm.Break />
+					<OptForm.Text>
+						Ready to watch? Enter your email to create or restart
+						your membership
+					</OptForm.Text>
+				</OptForm>
+			</HeaderContainer>
 			<JumbotronContainer />
 			<FaqsContainter />
 			<FooterContainer />
